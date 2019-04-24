abee80319f984ffd6b700241d44c8c30fd299148
Mundua Patrick <patrick.mundua@gmail.com>
2019-04-24 22:47:09 +0300

    Page Error Handling

--- X/notes.php
+++ X/notes.php
@@ -1,34 +1,39 @@
 <?php
-require ('../lib/xcrud/xcrud.php');
+require_once '../securex/extra/auth.php';
+$returnURL = 'X/offices';
+if (! Auth::check()) {
+    redirectTo('securex/public/login?to=' . $returnURL);
+    exit();
+}
+app(\Vanguard\Services\Logging\UserActivity\Logger::class)->log($returnURL);
+$username = Auth::user()->username;
 
+require ('../lib/xcrud/xcrud.php');
 $xcrud1 = Xcrud::get_instance();
 $xcrud1->table('stickynotes')
         ->table_name('Sticky Notes');
 $xcrud1->columns('title')
+        ->where("username=" , $username)
         ->relation('color','alerts','alert_class','alert_name')
         ->column_pattern('title','
             <div class="alert {color} container-fluid">
                 <div class="col-sm-9"><strong>{value}</strong><br>{notes}</div>
-                <div class="col-sm-3 pull-right"><strong class="pull-right">Date <i class="fa fa-clock-o"></i> {date}</strong><br>
+                <div class="col-sm-3 pull-right"><strong class="pull-right">Date <i class="fa fa-clock-o"></i> {date}</strong><br><br>
                 </div>
              </div>')
         ->order_by('id','asc');;
 $xcrud1->unset_view()->unset_limitlist()->limit(25)->unset_csv()->unset_print();
 $xcrud1->hide_button('add');
 
-
 echo $xcrud1->render();
-
-
-
-
 
 
 $xcrud2 = Xcrud::get_instance();
 $xcrud2->table('stickynotes')
         ->unset_title();
 $xcrud2->fields('title,notes,color,date')
-        ->relation('color','alerts','alert_class','alert_name');
+        ->relation('color','alerts','alert_class','alert_name')
+        ->pass_var('username', $username);
 $xcrud2->hide_button('save_return,return,save_edit');
 $xcrud2->set_lang('save_new','Publish New Note');
 echo $xcrud2->render('create');
